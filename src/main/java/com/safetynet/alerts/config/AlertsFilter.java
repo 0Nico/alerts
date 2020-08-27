68434d98cd024158eea1e393166a7d3e357df71f
Nicolas Dupuy <nicolas.dupuy@mimbus.com>
2020-08-28 00:58:24 +0200

    Fix requests and responses logging

--- src/main/java/com/safetynet/alerts/config/AlertsFilter.java
+++ src/main/java/com/safetynet/alerts/config/AlertsFilter.java
@@ -7,23 +7,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class AlertsFilter extends OncePerRequestFilter{
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
-		
+		ContentCachingResponseWrapper responseWrapper =new ContentCachingResponseWrapper(response);
+		ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+       
+       
 		try{
-			filterChain.doFilter(request, response);
-			logger.info(request);
+			filterChain.doFilter(requestWrapper, responseWrapper);
+			logger.info(">>> Ip adress of the client : " + requestWrapper.getLocalAddr());
 		}finally {
-			logger.info(response);
+			logger.info(">>> HTTP Response Status : " + responseWrapper.getStatus());
+			
+			
+			byte[] responseArray = responseWrapper.getContentAsByteArray();
+            String responseStr = new String(responseArray,responseWrapper.getCharacterEncoding());
+            
+			logger.info(">>> HTTP Response Body : " + responseStr);
+			responseWrapper.copyBodyToResponse();
+			
 		}
 	}
 	
